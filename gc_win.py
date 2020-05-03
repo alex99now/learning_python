0411399206f0da05b387aab66809000e6ce26d68
alex99now <agmerrell@ucdavis.edu>
2020-05-02 20:07:56 -0700

    Lesson 3

--- gc_win.py
+++ gc_win.py
@@ -3,17 +3,19 @@
 # Write a program that computes the GC fraction of a DNA sequence in a window
 # Window size is 11 nt
 # Output with 4 significant figures using whichever method you prefer
+# Use nested loops
 
 seq = 'ACGACGCAGGAGGAGAGTTTCAGAGATCACGAATACATCCATATTACCCAGAGAGAG'
 w = 11
 
-for a in range(0, len(seq)):
-    frame = seq[a:a + w]
-    if len(frame) < w:
-        break
-    cg = frame.count('C') + frame.count('G')
-    ans = float('%.4g' % (cg / w))
-    print(a, frame, ans)
+
+for i in range(len(seq) -w +1):
+    sseq = seq[i:i+w]
+    gc = 0
+    for nt in sseq:
+        if nt == 'G' or nt == 'C':
+            gc += 1
+    print(i, sseq, gc/w)
 
 """
 0 ACGACGCAGGA 0.6364
